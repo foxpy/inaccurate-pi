102d7c78ac21b34771addbd55652458df155c664
Murad <muradkarammaev@gmail.com>
2020-01-13 13:39:19 +0300

    incapsulate header to prevent multiple includes

--- src/random-interface.h
+++ src/random-interface.h
@@ -1,2 +1,7 @@
+#ifndef __INACCURATE_PI_RANDOM_INTERFACE_H
+#define __INACCURATE_PI_RANDOM_INTERFACE_H
+
 int range_randomd(double low, double high, double *dst);
 int range_randoms(signed low, signed high, signed *dst);
+
+#endif
