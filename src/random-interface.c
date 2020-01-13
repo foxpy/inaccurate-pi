a2a159c95ff6093307cbd82e112da08cd5a7f317
Murad <muradkarammaev@gmail.com>
2020-01-13 13:28:33 +0300

    implement random double generator

--- src/random-interface.c
+++ src/random-interface.c
@@ -5,11 +5,27 @@
 
 #include "random-interface.h"
 
+#define DOUBLE_ONE_EXPONENT 0x3ff0000000000000
+#define DOUBLE_FRACTION_MASK 0xfffffffffffff
 #define abs(x) ((x>=0)?x:-x)
 
-double range_randomd(double low, double high)
+union transparent_double {
+	double d;
+	uint64_t u;
+};
+typedef union transparent_double td;
+
+int range_randomd(double low, double high, double *dst)
 {
-	return low;
+	td val;
+
+	if (low >= high) return -1;
+	if (getrandom(&val.d, sizeof(val.d), 0) != sizeof(val.d)) return -1;
+	val.u &= DOUBLE_FRACTION_MASK;
+	val.u |= DOUBLE_ONE_EXPONENT;
+	val.d -= 1;
+	*dst = low + val.d * (high - low); // fuck it I need [low, high) but not [0.0F, 1.0F) >:(
+	return 0;
 }
 
 int range_randoms(signed low, signed high, signed *dst)
--- src/random-interface.h
+++ src/random-interface.h
@@ -1,2 +1,2 @@
-double range_randomd(double low, double high);
+int range_randomd(double low, double high, double *dst);
 int range_randoms(signed low, signed high, signed *dst);
