486ff0199204733a29a072889a76d1c3371c4d93
Murad <muradkarammaev@gmail.com>
2020-09-01 10:38:30 +0300

    update to upstream qc version: use autogen help message

--- inaccurate-pi.c
+++ inaccurate-pi.c
@@ -5,31 +5,21 @@
 #include <qc.h>
 #include "needle.h"
 
-static void help(void *help_data) {
-    char* program_name = help_data;
-    fprintf(stderr, "Usage: %s --length=DOUBLE --distance=DOUBLE --iterations=INTEGER\n", program_name);
-    fputs("Where:\n", stderr);
-    fputs("\t--length - length of needle\n", stderr);
-    fputs("\t--distance - distance between lines\n", stderr);
-    fputs("\t--iterations - number of needles dropped\n", stderr);
-}
-
 int main(int argc, char *argv[]) {
     double l = 0.0;
     double t = 0.0;
     size_t n = 0;
     qc_args* args = qc_args_new();
-    qc_args_set_help(args, help, argv[0]);
-    qc_args_double(args, "length", &l);
-    qc_args_double(args, "distance", &t);
-    qc_args_unsigned(args, "iterations", &n);
+    qc_args_double(args, "length", &l, "Length of needle");
+    qc_args_double(args, "distance", &t, "Distance between lines");
+    qc_args_unsigned(args, "iterations", &n, "Number of dropped needles");
     char* err;
     if (qc_args_parse(args, argc, argv, &err) == -1) {
         fprintf(stderr, "Failed to parse arguments: %s\n", err);
         free(err);
         exit(EXIT_FAILURE);
     } else if (l == 0.0 || t == 0.0 || n == 0) {
-        help(argv[0]);
+        qc_args_call_help(args);
         exit(EXIT_FAILURE);
     } else if (l <= DBL_EPSILON) {
         die("Fatal: l too small");
